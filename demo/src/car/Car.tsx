e13e1793138ed2d5ccd7b50312db8c9831d1d6d3
Edwin Webb <edwin.webb@gmail.com>
2022-06-24 11:31:37 -0700

    expand car demo to more verbose

--- demo/src/car/Car.tsx
+++ demo/src/car/Car.tsx
@@ -1,17 +1,21 @@
 import { Box, Cylinder } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { RigidBody, RigidBodyApi, useRevoluteJoint } from "@react-three/rapier";
+import { Debug, RigidBody, RigidBodyApi, useRevoluteJoint } from "@react-three/rapier";
 import { createRef, useRef } from "react";
 import { Demo } from "../App";
 
-const RevoluteJoint = ({ a, b, anchors }) => {
-  const joint = useRevoluteJoint(a, b, anchors);
+const RevoluteJoint = ({ body, wheel, anchors }) => {
+  const joint = useRevoluteJoint(body, wheel, anchors);
   return null;
 };
 
 export const Car: Demo = ({ setUI }) => {
   
-  const boxRef = useRef<RigidBodyApi>(null)
+  const bodyRef = useRef<RigidBodyApi>(null)
+  const frontLeftRef = useRef<RigidBodyApi>(null)
+  const frontRightRef = useRef<RigidBodyApi>(null)
+  const rearLeftRef = useRef<RigidBodyApi>(null)
+  const rearRightRef = useRef<RigidBodyApi>(null)
   const refs = useRef(
     Array.from({ length: 4 }).map(() => createRef<RigidBodyApi>())
   );
@@ -20,36 +24,79 @@ export const Car: Demo = ({ setUI }) => {
 
   useFrame(() => {
     refs.current.forEach((ref) => {
-      ref.current?.applyTorqueImpulse({x:0,y:0,z:.1})
+      //ref.current?.applyTorqueImpulse({x:0,y:0,z:.1})
     });
+    frontLeftRef.current?.applyTorqueImpulse({x:0,y:0,z:.1})
+    frontRightRef.current?.applyTorqueImpulse({x:0,y:0,z:.1})
+    rearLeftRef.current?.applyTorqueImpulse({x:0,y:0,z:.1})
+    rearRightRef.current?.applyTorqueImpulse({x:0,y:0,z:.1})
   });
+
+  //useRevoluteJoint(bodyRef, frontLeftRef, [[0,0,0],[0,0,0],[0,0,0]]);
 
   return ( 
     <group>
-      <RigidBody colliders="cuboid" position={[0,0,0]} ref={boxRef}>
-        <Box 
-          scale={[6,1,1]}
+      <Debug />
+      <RigidBody
+        colliders="cuboid"
+        ref={bodyRef}
+        type="dynamic"
+        >
+          <Box 
+          scale={[6,1,1.9]}
           castShadow
           receiveShadow
+          name="chassis"
          />
       </RigidBody>
-      {Array.from({ length: 4 }).map((_, i) => (
-        <RigidBody
-          position={[i % 2 ? -3 : 3, .5, i > 1 ? -2 : 2]}
-          rotation={[Math.PI/2, 0, 0]}
-          colliders="ball"
-          ref={refs.current[i]}
-          key={i}
+      <RigidBody
+        position={[-3, 0, -2]}
+        colliders="hull"
+        type="dynamic"
+        ref={frontLeftRef}
         >
-          <Cylinder 
-            castShadow 
-            receiveShadow
-          />
-        </RigidBody>
-      ))}
-      {Array.from({ length: 4 }).map((_, i) => (
-        <RevoluteJoint a={refs.current[i]} b={boxRef} key={i} anchors={[[0, i > 1 ? -2 : 2, 0], [i % 2 ? -3 : 3, 0, 0], [0, 1, 0]]} />
-      ))}    
+        <Cylinder 
+          rotation={[Math.PI/2, 0, 0]}
+          args={[1,1,1,32]}
+        />
+      </RigidBody>
+      <RevoluteJoint body={bodyRef} wheel={frontLeftRef}  anchors={[[-3, 0, -2], [0,0,0], [0, 0, 1]]} />
+      <RigidBody
+        position={[-3, 0, 2]}
+        colliders="hull"
+        type="dynamic"
+        ref={frontRightRef}
+        >
+        <Cylinder 
+          rotation={[Math.PI/2, 0, 0]}
+          args={[1,1,1,32]}
+        />
+      </RigidBody>
+      <RevoluteJoint body={bodyRef} wheel={frontRightRef} anchors={[[-3, 0, 2], [0,0,0], [0, 0, 1]]} />
+      <RigidBody
+        position={[3, 0, -2]}
+        colliders="hull"
+        type="dynamic"
+        ref={rearLeftRef}
+        >
+        <Cylinder 
+          rotation={[Math.PI/2, 0, 0]}
+          args={[1,1,1,32]}
+        />
+      </RigidBody>
+      <RevoluteJoint body={bodyRef} wheel={rearLeftRef}  anchors={[[3, 0, -2], [0,0,0], [0, 0, 1]]} />
+      <RigidBody
+        position={[3, 0, 2]}
+        colliders="hull"
+        type="dynamic"
+        ref={rearRightRef}
+        >
+        <Cylinder 
+          rotation={[Math.PI/2, 0, 0]}
+          args={[1,1,1,32]}
+        />
+      </RigidBody>
+      <RevoluteJoint body={bodyRef} wheel={rearRightRef} anchors={[[3, 0, 2], [0,0,0], [0, 0, 1]]} />
     </group>
   );
 };
